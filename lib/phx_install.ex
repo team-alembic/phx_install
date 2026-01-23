52b17af5657c8d5d93568c3668e1fb8eea6ec674
James Harton <james@harton.nz>
2026-01-23 15:33:53 +1300

    chore: empty app

---
+++ lib/phx_install.ex
@@ -0,0 +1,18 @@
+defmodule PhxInstall do
+  @moduledoc """
+  Documentation for `PhxInstall`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxInstall.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_install_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxInstallTest do
+  use ExUnit.Case
+  doctest PhxInstall
+
+  test "greets the world" do
+    assert PhxInstall.hello() == :world
+  end
+end
